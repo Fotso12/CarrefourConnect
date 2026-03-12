aa7f4ff21ffeba7da8c4e9fa627fb205eae0a1ad
Fotso12 <tamofotso90@gmail.com>
2026-03-12 09:33:35 +0100

    Initialisation du projet d'annuaire numérique intéractif des commerces

---
+++ src/main/java/com/carrefourconnect/CarrefourconnectApplication.java
@@ -0,0 +1,13 @@
+package com.carrefourconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrefourconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrefourconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrefourconnect/CarrefourconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrefourconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrefourconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
